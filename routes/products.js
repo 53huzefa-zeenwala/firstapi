ee984eceafc95054cafb135f2598b2dc3f17fa88
53huzefa-zeenwala <huzefa.zeenwal@gmail.com>
2022-11-07 16:11:45 +0530

    change in api route

--- routes/products.js
+++ routes/products.js
@@ -28,15 +28,50 @@ router.post("/", async (req, res) => {
 });
 
 // getting products
+// router.get("/", async (req, res) => {
+//   const query = req.query;
+//   console.log(query)
+//   try {
+//     if (query.page === "all") {
+//       const products = await Product.find();
+//       res.status(200).json(products);
+//     } else if (query.category || query.frametype) {
+//       const products = await Product.find({
+//         $or: [{ category: query.category }, { frametype: query.frametype }],
+//       })
+//         .limit(4)
+//         .skip(query.page * 4);
+//       res.status(200).json(products);
+//     } else {
+//       const products = await Product.find()
+//         .limit(4)
+//         .skip(query.page * 4);
+//       res.status(200).json(products);
+//     }
+//   } catch (error) {
+//     res.status(400).json({ message: error });
+//   }
+// });
+
 router.get("/", async (req, res) => {
   const query = req.query;
+  const categoryQuery = () => {
+    const categories = ['bag', 'specticles']
+    return query.category ? categories.filter(category => category === query.category) : categories
+  }
+  const frametypeQuery = () => {
+    const frametypes = ['slim', 'cateye']
+    return query.frametype ? frametypes.filter(frametype => frametype === query.frametype) : frametypes
+  }
+
+  console.log(query, categoryQuery(), frametypeQuery())
   try {
     if (query.page === "all") {
       const products = await Product.find();
       res.status(200).json(products);
     } else if (query.category || query.frametype) {
       const products = await Product.find({
-        $or: [{ category: query.category }, { frametype: query.frametype }],
+        $and : [{ category: { $in: categoryQuery() } }, { frametype: { $in: frametypeQuery() } }]
       })
         .limit(4)
         .skip(query.page * 4);
@@ -51,5 +86,16 @@ router.get("/", async (req, res) => {
     res.status(400).json({ message: error });
   }
 });
+
+
+// deleing single post
+router.delete('/:productId', async (req, res) => {
+  try {
+      const deleteProduct = await Product.remove({ _id: req.params.productId })
+      res.status(200).json(deleteProduct)
+  } catch (error) {
+      res.status(400).json({ message: error })
+  }
+})
 
 module.exports = router;
